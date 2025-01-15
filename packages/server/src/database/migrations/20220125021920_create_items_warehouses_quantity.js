718a136e8bf6500c14f38c3b8193e39b63c65ba5
Aditya <aditya00jangir@gmail.com>
2025-01-15 10:54:09 +0530

    changes in migration of warehouse

--- packages/server/src/database/migrations/20220125021920_create_items_warehouses_quantity.js
+++ packages/server/src/database/migrations/20220125021920_create_items_warehouses_quantity.js
@@ -1,5 +1,6 @@
 exports.up = (knex) => {
   return knex.schema.createTable('items_warehouses_quantity', (table) => {
+    table.increments('id');
     table.integer('item_id').unsigned().references('id').inTable('items');
     table
       .integer('warehouse_id')
