d035a2a8dff54f913836e2975552afefba271ab1
Aditya <aditya00jangir@gmail.com>
2025-02-19 09:29:53 +0530

    add logs

--- packages/server/src/utils/taxRate.ts
+++ packages/server/src/utils/taxRate.ts
@@ -5,6 +5,14 @@
  * @returns {number}
  */
 export const getInclusiveTaxAmount = (amount: number, taxRate: number) => {
+  console.log("this is type of tax",typeof taxRate)
+  console.log("this is  tax", taxRate)
+  if(typeof taxRate === 'string')
+  {
+    taxRate = Number(taxRate);
+  }
+  console.log("this is type of tax",typeof taxRate)
+  console.log("this is  tax", taxRate)
   return (amount * taxRate) / (100 + taxRate);
 };
 
