4c714b8e8a50169c46901ed09c4d9cfbb9305677
Aditya <aditya00jangir@gmail.com>
2025-02-19 10:34:31 +0530

    fix responsive issue in payment portal invoice

--- packages/webapp/src/containers/PaymentPortal/PaymentPortal.tsx
+++ packages/webapp/src/containers/PaymentPortal/PaymentPortal.tsx
@@ -63,158 +63,162 @@ export function PaymentPortal() {
   };
 
   return (
-    <Box className={styles.root} my={'40px'} mx={'auto'}>
-      <Stack spacing={0} className={styles.body}>
-        <Stack>
-          <Group spacing={10}>
-            {sharableLinkMeta?.brandingTemplate?.companyLogoUri && (
-              <Box
-                className={styles.companyLogoWrap}
-                style={{
-                  backgroundImage: `url(${sharableLinkMeta?.brandingTemplate?.companyLogoUri})`,
-                }}
-              ></Box>
-            )}
-            <Text>{sharableLinkMeta?.organization?.name}</Text>
-          </Group>
-
-          <Stack spacing={6}>
-            <h1 className={styles.bigTitle}>
-              {sharableLinkMeta?.organization?.name} Sent an Invoice for{' '}
-              {sharableLinkMeta?.totalFormatted}
-            </h1>
+    // make it responsive for mobile add div and comment out box
+    <div className={styles.mobileOnly}> 
+      {/* <Box className={styles.root} my={'40px'} mx={'auto'}>  */}
+        <Stack spacing={0} className={styles.body}>
+          <Stack>
             <Group spacing={10}>
-              <Text className={clsx(Classes.TEXT_MUTED, styles.invoiceDueDate)}>
-                Invoice due {sharableLinkMeta?.dueDateFormatted}{' '}
-              </Text>
-            </Group>
-          </Stack>
-
-          <Stack className={styles.address} spacing={2}>
-            <Box className={styles.customerName}>
-              {sharableLinkMeta?.customerName}
-            </Box>
-
-            {sharableLinkMeta?.formattedCustomerAddress && (
-              <Box
-                dangerouslySetInnerHTML={{
-                  __html: sharableLinkMeta?.formattedCustomerAddress,
-                }}
-              />
-            )}
-          </Stack>
-
-          <h2 className={styles.invoiceNumber}>
-            Invoice {sharableLinkMeta?.invoiceNo}
-          </h2>
-
-          <Stack spacing={0} className={styles.totals}>
-            <Group
-              position={'apart'}
-              className={clsx(styles.totalItem, styles.borderBottomGray)}
-            >
-              <Text>Sub Total</Text>
-              <Text>{sharableLinkMeta?.subtotalFormatted}</Text>
+              {sharableLinkMeta?.brandingTemplate?.companyLogoUri && (
+                <Box
+                  className={styles.companyLogoWrap}
+                  style={{
+                    backgroundImage: `url(${sharableLinkMeta?.brandingTemplate?.companyLogoUri})`,
+                  }}
+                ></Box>
+              )}
+              <Text>{sharableLinkMeta?.organization?.name}</Text>
             </Group>
 
-            <Group position={'apart'} className={styles.totalItem}>
-              <Text>Total</Text>
-              <Text style={{ fontWeight: 500 }}>
+            <Stack spacing={6}>
+              <h1 className={styles.bigTitle}>
+                {sharableLinkMeta?.organization?.name} Sent an Invoice for{' '}
                 {sharableLinkMeta?.totalFormatted}
-              </Text>
-            </Group>
-
-            {sharableLinkMeta?.taxes?.map((tax, key) => (
-              <Group key={key} position={'apart'} className={styles.totalItem}>
-                <Text>{tax?.name}</Text>
-                <Text>{tax?.taxRateAmountFormatted}</Text>
+              </h1>
+              <Group spacing={10}>
+                <Text className={clsx(Classes.TEXT_MUTED, styles.invoiceDueDate)}>
+                  Invoice due {sharableLinkMeta?.dueDateFormatted}{' '}
+                </Text>
               </Group>
-            ))}
-            <Group
-              position={'apart'}
-              className={clsx(styles.totalItem, styles.borderBottomGray)}
-            >
-              <Text>Paid Amount (-)</Text>
-              <Text>{sharableLinkMeta?.paymentAmountFormatted}</Text>
-            </Group>
+            </Stack>
 
-            <Group
-              position={'apart'}
-              className={clsx(styles.totalItem, styles.borderBottomDark)}
+            <Stack className={styles.address} spacing={2}>
+              <Box className={styles.customerName}>
+                {sharableLinkMeta?.customerName}
+              </Box>
+
+              {sharableLinkMeta?.formattedCustomerAddress && (
+                <Box
+                  dangerouslySetInnerHTML={{
+                    __html: sharableLinkMeta?.formattedCustomerAddress,
+                  }}
+                />
+              )}
+            </Stack>
+
+            <h2 className={styles.invoiceNumber}>
+              Invoice {sharableLinkMeta?.invoiceNo}
+            </h2>
+
+            <Stack spacing={0} className={styles.totals}>
+              <Group
+                position={'apart'}
+                className={clsx(styles.totalItem, styles.borderBottomGray)}
+              >
+                <Text>Sub Total</Text>
+                <Text>{sharableLinkMeta?.subtotalFormatted}</Text>
+              </Group>
+
+              <Group position={'apart'} className={styles.totalItem}>
+                <Text>Total</Text>
+                <Text style={{ fontWeight: 500 }}>
+                  {sharableLinkMeta?.totalFormatted}
+                </Text>
+              </Group>
+
+              {sharableLinkMeta?.taxes?.map((tax, key) => (
+                <Group key={key} position={'apart'} className={styles.totalItem}>
+                  <Text>{tax?.name}</Text>
+                  <Text>{tax?.taxRateAmountFormatted}</Text>
+                </Group>
+              ))}
+              <Group
+                position={'apart'}
+                className={clsx(styles.totalItem, styles.borderBottomGray)}
+              >
+                <Text>Paid Amount (-)</Text>
+                <Text>{sharableLinkMeta?.paymentAmountFormatted}</Text>
+              </Group>
+
+              <Group
+                position={'apart'}
+                className={clsx(styles.totalItem, styles.borderBottomDark)}
+              >
+                <Text>Due Amount</Text>
+                <Text style={{ fontWeight: 500 }}>
+                  {sharableLinkMeta?.dueAmountFormatted}
+                </Text>
+              </Group>
+            </Stack>
+          </Stack>
+
+          <Stack spacing={8} className={styles.footerButtons}>
+            <Button
+              minimal
+              className={clsx(styles.footerButton, styles.downloadInvoiceButton)}
+              onClick={handleInvoiceDownloadBtnClick}
+              loading={isInvoiceGenerating}
             >
-              <Text>Due Amount</Text>
-              <Text style={{ fontWeight: 500 }}>
-                {sharableLinkMeta?.dueAmountFormatted}
-              </Text>
-            </Group>
+              Download Invoice
+            </Button>
+
+            <Button
+              onClick={handleInvoicePreviewBtnClick}
+              className={clsx(styles.footerButton, styles.viewInvoiceButton)}
+            >
+              View Invoice
+            </Button>
+
+            {sharableLinkMeta?.isReceivable &&
+              sharableLinkMeta?.hasStripePaymentMethod && (
+                <Button
+                  intent={Intent.PRIMARY}
+                  className={clsx(
+                    styles.footerButton,
+                    styles.buyButton,
+                    css`
+                      &.bp4-button.bp4-intent-primary {
+                        background-color: var(--payment-page-primary-button);
+
+                        &:hover,
+                        &:focus {
+                          background-color: var(
+                            --payment-page-primary-button-hover
+                          );
+                        }
+                      }
+                    `,
+                  )}
+                  loading={isStripeCheckoutLoading}
+                  onClick={handlePayButtonClick}
+                >
+                  Pay {sharableLinkMeta?.totalFormatted}
+                </Button>
+              )}
+          </Stack>
+
+          <Text className={clsx(Classes.TEXT_MUTED, styles.buyNote)}>
+            By confirming your payment, you allow Bigcapital Technology, Inc. to
+            charge you for this payment and save your payment information in
+            accordance with their terms.
+          </Text>
+        </Stack>
+
+        <Stack spacing={18} className={styles.footer}>
+          <Box
+            dangerouslySetInnerHTML={{
+              __html: sharableLinkMeta?.organization?.addressTextFormatted || '',
+            }}
+          ></Box>
+
+          <Stack spacing={0} className={styles.footerText}>
+            © 2024 Bigcapital Technology, Inc.
+            <br />
+            All rights reserved.
           </Stack>
         </Stack>
-
-        <Stack spacing={8} className={styles.footerButtons}>
-          <Button
-            minimal
-            className={clsx(styles.footerButton, styles.downloadInvoiceButton)}
-            onClick={handleInvoiceDownloadBtnClick}
-            loading={isInvoiceGenerating}
-          >
-            Download Invoice
-          </Button>
-
-          <Button
-            onClick={handleInvoicePreviewBtnClick}
-            className={clsx(styles.footerButton, styles.viewInvoiceButton)}
-          >
-            View Invoice
-          </Button>
-
-          {sharableLinkMeta?.isReceivable &&
-            sharableLinkMeta?.hasStripePaymentMethod && (
-              <Button
-                intent={Intent.PRIMARY}
-                className={clsx(
-                  styles.footerButton,
-                  styles.buyButton,
-                  css`
-                    &.bp4-button.bp4-intent-primary {
-                      background-color: var(--payment-page-primary-button);
-
-                      &:hover,
-                      &:focus {
-                        background-color: var(
-                          --payment-page-primary-button-hover
-                        );
-                      }
-                    }
-                  `,
-                )}
-                loading={isStripeCheckoutLoading}
-                onClick={handlePayButtonClick}
-              >
-                Pay {sharableLinkMeta?.totalFormatted}
-              </Button>
-            )}
-        </Stack>
-
-        <Text className={clsx(Classes.TEXT_MUTED, styles.buyNote)}>
-          By confirming your payment, you allow Bigcapital Technology, Inc. to
-          charge you for this payment and save your payment information in
-          accordance with their terms.
-        </Text>
-      </Stack>
-
-      <Stack spacing={18} className={styles.footer}>
-        <Box
-          dangerouslySetInnerHTML={{
-            __html: sharableLinkMeta?.organization?.addressTextFormatted || '',
-          }}
-        ></Box>
-
-        <Stack spacing={0} className={styles.footerText}>
-          © 2024 Bigcapital Technology, Inc.
-          <br />
-          All rights reserved.
-        </Stack>
-      </Stack>
-    </Box>
+      {/* </Box> */}
+    </div>
   );
+  
 }
